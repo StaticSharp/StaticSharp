be5e2f6c30f861ddbbf08cb0ce5a5521141748d4
pavel.zybenkov <pavel.zybenkov@antilatency.com>
2023-01-10 16:45:47 +0300

    MixinGenerator executable fixed, Debugger output saved to file

--- RoslynSourceGeneratorDebugger/Program.cs
+++ RoslynSourceGeneratorDebugger/Program.cs
@@ -4,6 +4,8 @@ using MixinGenerator;
 using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.Build.Locator;
 using System.Runtime.CompilerServices;
+using System.IO;
+using System.Threading;
 
 public class Program
 {
@@ -21,10 +23,29 @@ public class Program
 
         driver = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation, out var diagnostics);
 
-        var result = driver.GetRunResult();
-        var generationResult = result.Results.FirstOrDefault().GeneratedSources.FirstOrDefault();
+        var firstGeneratorResult = driver.GetRunResult().Results.FirstOrDefault();
+        var generationResult = firstGeneratorResult.GeneratedSources.FirstOrDefault();
 
-        Console.WriteLine(generationResult.HintName);
+        var dataToSave = firstGeneratorResult.GeneratedSources.ToDictionary(_ => _.HintName, _ => _.SourceText.ToString());
+
+        var outputDirectory = $"{GetPath()}/../StaticSharp/.generated/Debugger";
+        var outputDirectoryInfo = Directory.CreateDirectory(outputDirectory);
+        var filesToDelete = outputDirectoryInfo.GetFiles("*.cs", SearchOption.AllDirectories).Select(x => x.FullName).ToList();
+
+
+        foreach (var r in dataToSave)
+        {
+            var outputPath = Path.Combine(outputDirectory, r.Key);
+            filesToDelete.Remove(outputPath);
+            File.WriteAllText(outputPath, r.Value);
+        }
+
+        foreach (var f in filesToDelete)
+        {
+            File.Delete(f);
+        }
+
+        
     }
 
     private static string GetPath([CallerFilePath] string path = "")
