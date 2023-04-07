9fa39651382ae79309c4e30084c0b4419545db6d
pavel.zybenkov <pavel.zybenkov@antilatency.com>
2023-04-07 13:45:16 +0300

    StaticSharpComponentSourceGenerator added

---
+++ StaticSharpComponentSourceGenerator/Generator/StaticSharpComponentSourceGenerator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace StaticSharpComponentSourceGenerator
+{
+    [Microsoft.CodeAnalysis.Generator]
+    public class StaticSharpComponentSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("Generated.cs", $@"
+public static class HelloClass
+{{
+    public static string HelloString = ""Hello from {nameof(StaticSharpComponentSourceGenerator)}!"";
+}}
+");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
+}
---
+++ StaticSharpComponentSourceGenerator/Launcher/Program.cs
@@ -0,0 +1,12 @@
+﻿
+public class Program
+{
+    public static async Task Main()
+    {
+        var targetProjectPath = Path.Combine(ProjectDirectory.Path, "..\\..\\StaticSharp\\StaticSharp.csproj");
+        var outputPath = Path.Combine(Path.GetDirectoryName(targetProjectPath), 
+            $".generated/{typeof(StaticSharpComponentSourceGenerator.StaticSharpComponentSourceGenerator).FullName}");
+        await RoslynSourceGeneratorLauncher.RoslynSourceGeneratorLauncher.Launch(
+            new StaticSharpComponentSourceGenerator.StaticSharpComponentSourceGenerator(), targetProjectPath, outputPath);
+    }
+}
