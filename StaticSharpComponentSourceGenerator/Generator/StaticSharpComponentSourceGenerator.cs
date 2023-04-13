eebce30fd64c44b97045b9e96fd512d1a0d12a77
Petr Sevostianov <ps@antilatency.com>
2023-04-13 13:19:15 +0300

    -

--- StaticSharpComponentSourceGenerator/Generator/StaticSharpComponentSourceGenerator.cs
+++ StaticSharpComponentSourceGenerator/Generator/StaticSharpComponentSourceGenerator.cs
@@ -1,14 +1,51 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
 using System;
+using System.Linq;
 
-namespace StaticSharpComponentSourceGenerator
-{
+namespace StaticSharpComponentSourceGenerator {
+
+
+    
+
     [Microsoft.CodeAnalysis.Generator]
-    public class StaticSharpComponentSourceGenerator : ISourceGenerator
-    {
-        public void Execute(GeneratorExecutionContext context)
-        {
-            context.AddSource("Generated.cs", $@"
+    public class StaticSharpComponentSourceGenerator : ISourceGenerator {
+
+        static bool IsInheritFrom(INamedTypeSymbol type, INamedTypeSymbol baseType) {
+            INamedTypeSymbol currentType = type;
+            while (currentType != null) {
+                if (currentType.BaseType != null && SymbolEqualityComparer.Default.Equals(currentType.BaseType, baseType)) {
+                    return true;
+                }
+                currentType = currentType.BaseType;
+            }
+            return false;
+        }
+
+
+        public void Execute(GeneratorExecutionContext context) {
+
+            var allSymbols = context.Compilation.GetSymbolsWithName(_ => true);
+            var typeSymbols = allSymbols.OfType<INamedTypeSymbol>();
+
+            var entityType = typeSymbols.First(x => (x.ContainingNamespace + "." + x.MetadataName) == "StaticSharp.Gears.Entity");
+            
+            var partialTypes = typeSymbols.Where(x=>
+               ((TypeDeclarationSyntax)x.DeclaringSyntaxReferences.First().GetSyntax())
+               .Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword))
+               );
+
+            var entityTypes = partialTypes.Where(x => IsInheritFrom(x, entityType));
+
+
+            foreach (var i in entityTypes) {
+                ProcessClass(i, context);
+            }
+
+
+
+                context.AddSource("Generated.cs", $@"
 public static class HelloClass
 {{
     public static string HelloString = ""Hello from {nameof(StaticSharpComponentSourceGenerator)}!"";
@@ -16,9 +53,13 @@ public static class HelloClass
 ");
         }
 
-        public void Initialize(GeneratorInitializationContext context)
-        {
+        public void ProcessClass(INamedTypeSymbol type, GeneratorExecutionContext context) { 
             
+        }
+
+
+        public void Initialize(GeneratorInitializationContext context) {
+
         }
     }
 }
