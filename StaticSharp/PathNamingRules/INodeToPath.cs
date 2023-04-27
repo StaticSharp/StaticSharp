588d157df37364d5a5ee34e8f3840e7d9fa4230a
Pavel Zybenkov <pavel_zybenkov@mail.ru>
2023-04-27 20:05:14 +0300

    Microsoft.CodeAnalysis.CSharp versions consolidation, DefaultMultilanguageNodeToPath

--- StaticSharp/PathNamingRules/INodeToPath.cs
+++ StaticSharp/PathNamingRules/INodeToPath.cs
@@ -2,7 +2,8 @@
 using System;
 using System.Linq;
 
-namespace StaticSharp {
+namespace StaticSharp
+{
     public interface INodeToPath {
         FilePath NodeToRelativeDirectory(Node node);
         FilePath NodeToRelativePath(Node node);
@@ -14,20 +15,6 @@ namespace StaticSharp {
             var result = NodeToRelativePath(node);
             result.Items[result.Items.Length - 1] += ".html";
             return result;
-        }
-    }
-
-    public class DefaultMultilanguageNodeToPath<LanguageEnum> : INodeToPath where LanguageEnum : struct, Enum {
-        public FilePath NodeToRelativeDirectory(Node node) {
-            return new(node.Path);            
-        }
-
-        public FilePath NodeToRelativePath(Node node) {
-            var path = NodeToRelativeDirectory(node);
-            if (node is MultilanguageProtoNode<LanguageEnum> protoNode) {
-                path += protoNode.Language.ToString().ToLower();
-            }
-            return path;
         }
     }
 
